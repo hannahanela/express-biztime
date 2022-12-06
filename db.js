54b5e8461ff0e6c9e39be773c44384d62f55dcf7
anela <anela89@gmail.com>
2022-12-06 12:14:10 -0800

    Connected Express app to PSQL databases

--- db.js
+++ db.js
@@ -1,2 +1,22 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+require("dotenv").config();
+
+/** Setup for Windows environment. */
+const DB_URI = process.env.NODE_ENV === "test"
+    ? DATABASE_TEST
+    : DATABASE;
+
+/** MAC setup. */
+// const DB_URI = process.env.NODE_ENV === "test"
+//    ? "postgresql:///biztime_2_test"
+//    : "postgresql:///biztime_2";
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
