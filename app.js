bbfeeb5b16ab890840453afe7a92c56b4ad5f150
anela <anela89@gmail.com>
2022-12-09 15:57:02 -0800

    Setup middleware for companies routes

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+"use strict";
+
 /** BizTime express application. */
 
 const express = require("express");
 const { NotFoundError } = require("./expressError");
+const companiesRoutes = require("./routes/companies");
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/companies", companiesRoutes);
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
@@ -20,7 +24,5 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
   return res.status(status).json({ error: { message, status } });
 });
-
-
 
 module.exports = app;
