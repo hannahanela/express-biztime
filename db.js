1eafc08d38154178fc42a37c8c8e5465d28593c6
anela <anela89@gmail.com>
2022-12-09 12:01:05 -0800

    Updated environmental variables to use dotenv

--- db.js
+++ db.js
@@ -5,16 +5,16 @@ require("dotenv").config();
 
 /** Setup for Windows environment. */
 const DB_URI = process.env.NODE_ENV === "test"
-    ? DATABASE_TEST
-    : DATABASE;
+  ? process.env.DATABASE_TEST
+  : process.env.DATABASE;
 
 /** MAC setup. */
 // const DB_URI = process.env.NODE_ENV === "test"
-//    ? "postgresql:///biztime_2_test"
-//    : "postgresql:///biztime_2";
+//   ? "postgresql:///biztime_2_test"
+//   : "postgresql:///biztime_2";
 
 let db = new Client({
-    connectionString: DB_URI
+  connectionString: DB_URI
 });
 
 db.connect();
