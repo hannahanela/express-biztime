892e693d8172e837f73381b947f2876ad30d91b9
anela <anela89@gmail.com>
2022-12-09 16:49:35 -0800

    Wrote route to GET data for a single company
    Passes tests for route.

--- routes/companies.js
+++ routes/companies.js
@@ -7,10 +7,10 @@ const db = require("../db");
 const { NotFoundError, BadRequestError } = require("../expressError");
 const router = new express.Router();
 
-/** GET /companies - get list of companies 
+/** GET /companies - get list of companies
  *    return '{companies: [company, ]}'
  *      with company as {code, name, description}
-*/
+ */
 router.get("/", async function (req, res, next) {
   const results = await db.query(
     `SELECT code, name, description
@@ -18,6 +18,24 @@ router.get("/", async function (req, res, next) {
   );
   const companies = results.rows;
   return res.json({ companies });
+});
+
+/** GET /companies/:code - get data about one company
+ *    return '{company: {code, name, description} }'
+ */
+router.get("/:code", async function (req, res, next) {
+  const code = req.params.code;
+  const results = await db.query(
+    `SELECT code, name, description
+            FROM companies
+            WHERE code = $1`,
+    [code]
+  );
+  const company = results.rows[0];
+  if (!company) {
+    throw new NotFoundError("No such company");
+  }
+  return res.json({ company });
 });
 
 module.exports = router;
