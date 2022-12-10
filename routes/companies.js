b2188d924190d496c53b330353ad749d4756764f
anela <anela89@gmail.com>
2022-12-09 17:14:17 -0800

    Wrote route for POST a new company
    Passes 2 of 3 tests. Need to throw error if company already exists

--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,7 @@
 /** Routes for companies */
 
 const express = require("express");
+const { route } = require("../app");
 const db = require("../db");
 const { NotFoundError, BadRequestError } = require("../expressError");
 const router = new express.Router();
@@ -25,17 +26,40 @@ router.get("/", async function (req, res, next) {
  */
 router.get("/:code", async function (req, res, next) {
   const code = req.params.code;
-  const results = await db.query(
+  const result = await db.query(
     `SELECT code, name, description
             FROM companies
             WHERE code = $1`,
     [code]
   );
-  const company = results.rows[0];
+  const company = result.rows[0];
   if (!company) {
     throw new NotFoundError("No such company");
   }
   return res.json({ company });
+});
+
+/** POST /companies - create new company from data
+ *    return '{company: {code, name, description}}'
+ */
+router.post("/", async function (req, res, next) {
+  const { code, name, description } = req.body;
+
+  if (!code || !name || !description) {
+    throw new BadRequestError("Missing required data");
+  }
+
+  // TODO: throw error for duplicate
+
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+            VALUES ($1, $2, $3)
+            RETURNING code, name, description`,
+    [code, name, description]
+  );
+  const company = result.rows[0];
+  console.log("result=", result);
+  return res.status(201).json({ company });
 });
 
 module.exports = router;
